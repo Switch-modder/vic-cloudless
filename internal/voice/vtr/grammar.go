87dd62272bac01968e02b8f2809b740f187bba6b
Kerigan <kerigancreighton@gmail.com>
2025-08-02 02:01:06 -0500

    Add photo and picture to grammar list

--- internal/voice/vtr/grammar.go
+++ internal/voice/vtr/grammar.go
@@ -30,7 +30,7 @@ func GetGrammerList(lang string) string {
 	// 		}
 	// 	}
 	// }
-	wordsList = append(wordsList, []string{"hello", "my name is", "whats", "weather", "my name", "what is", "set your", "eye color", "volume", "to", "how", "old", "are you", "start", "stop", "exploring", "go", "home", "to sleep", "good", "robot", "never", "mind", "morning", "afternoon", "evening", "night", "bye", "goodbye", "happy", "new year", "holidays", "sign in to alexa", "sign out of alexa", "move", "forward", "backwards", "turn", "around", "left", "right", "do", "pop", "a wheelie", "a wheelstand", "fistbump", "give me", "blackjack", "play", "yes", "no", "bad", "i'm", "i am", "sorry", "down", "up", "medium", "low", "high", "look at me", "shut", "love", "you", "have", "a", "question", "check", "the", "timer", "for", "what", "it", "time", "dance", "hit", "stand"}...)
+	wordsList = append(wordsList, []string{"photo", "picture", "hello", "my name is", "whats", "weather", "my name", "what is", "set your", "eye color", "volume", "to", "how", "old", "are you", "start", "stop", "exploring", "go", "home", "to sleep", "good", "robot", "never", "mind", "morning", "afternoon", "evening", "night", "bye", "goodbye", "happy", "new year", "holidays", "sign in to alexa", "sign out of alexa", "move", "forward", "backwards", "turn", "around", "left", "right", "do", "pop", "a wheelie", "a wheelstand", "fistbump", "give me", "blackjack", "play", "yes", "no", "bad", "i'm", "i am", "sorry", "down", "up", "medium", "low", "high", "look at me", "shut", "love", "you", "have", "a", "question", "check", "the", "timer", "for", "what", "it", "time", "dance", "hit", "stand"}...)
 	// add words in localization
 	for _, str := range ALL_STR {
 		text := GetText(str)
